3172a8d8a09a88aef66bda8a29a4f88c09577c4b
Neil Goodgame <neil.goodgame@genomicsengland.co.uk>
2019-06-26 09:19:37 +0100

    BERTHA-3529 #done made variants an instance variable

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 setup(
     name='vcf-dedup',
-    version='0.4.8',
+    version='0.4.9',
     description='Removal of duplicated variants from a VCF',
     packages=find_packages(),
     scripts=['scripts/vcf_dedupper'],
     url='https://github.com/genomicsengland/vcf-dedup',
-    download_url='https://github.com/genomicsengland/vcf-dedup/archive/v0.4.8.tar.gz',
+    download_url='https://github.com/genomicsengland/vcf-dedup/archive/v0.4.9.tar.gz',
     license='Apache',
     author='Pablo Riesgo Ferreiro',
     author_email='pablo.riesgo-ferreiro@genomicsengland.co.uk',
